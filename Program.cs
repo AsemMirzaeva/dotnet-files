73eae2c81893d555ced3e45773389f61249afe63
Asem <asemmirzaeva1@gmail.com>
2023-12-27 01:22:32 +0500

    new functions and classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+
+public struct MutablePoint
+{
+    public int X;
+    public int Y;
+
+    public MutablePoint(int x, int y) => (X, Y) = (x, y);
+    public override string ToString() => $"({X}, {Y})";
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var p1 = new MutablePoint(1, 2);
+        var p2 = p1;
+        p2.Y = 200;
+        Console.WriteLine($"{nameof(p1)} after{nameof(p2)} is modified: {p1}");
+        Console.WriteLine($"{nameof(p2)}: {p2}");
+
+        MutateAndDisplay(p2);
+        Console.WriteLine($"{nameof(p2)} after passing to a method: {p2}");
+
+    }
+
+    private static void MutateAndDisplay(MutablePoint p)
+    {
+        p.X = 100;
+        Console.WriteLine($"Point mutated in a method: {p}");
+    }
+}   
