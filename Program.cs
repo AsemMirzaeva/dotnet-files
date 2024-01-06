58f42718149266e42b05d26a1991c21ed61da752
Asem <asemmirzaeva1@gmail.com>
2024-01-06 22:05:55 +0500

    Using doubles

--- Program.cs
+++ Program.cs
@@ -1,36 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
+Console.WriteLine("Using doubles:");
+double a = 0.1;
+double b = 0.2;
 
-using System;
-
-public struct MutablePoint
+if (a + b == 0.3)
 {
-    public int X;
-    public int Y;
-
-    public MutablePoint(int x, int y) => (X, Y) = (x, y);
-    public override string ToString() => $"({X}, {Y})";
+    Console.WriteLine($"{a} + {b} equals {0.3}");
 }
 
-public class Program
+else
 {
-    public static void Main()
-    {
-        var p1 = new MutablePoint(1, 2);
-        var p2 = p1;
-        p2.Y = 200;
-        Console.WriteLine($"{nameof(p1)} after{nameof(p2)} is modified: {p1}");
-        Console.WriteLine($"{nameof(p2)}: {p2}");
+    Console.WriteLine($"{a} + {b} doesn NOT equal {0.3}");
+}
 
-        MutateAndDisplay(p2);
-        Console.WriteLine($"{nameof(p2)} after passing to a method: {p2}");
 
-    }
-
-    private static void MutateAndDisplay(MutablePoint p)
-    {
-        p.X = 100;
-        Console.WriteLine($"Point mutated in a method: {p}");
-    }
-}   
